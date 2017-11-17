f3e2c9003be7907ab5955cbf94aa8757f18e2bef
Gogh-office <4535292@qq.com>
2017-11-17 20:11:43 +0800

    day1 account index, account setting, account setting-changePhoto

---
+++ pages/account/index.js
@@ -0,0 +1,97 @@
+// pages/account/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    signCheck: false,
+    signIn: false,
+    getSilver: 33,
+    date: new Date().getDate(),
+    accountInfo: {
+      photo: 'https://match.steampp.com/mobile/static/wx/account/accountPhoto.png',
+      name : '黑土heyt111111111111111',
+      level: '掘金游民',
+      levelBg: 'https://match.steampp.com/mobile/static/wx/account/accountLevel.png',
+      silver: 55512,
+      VIPprops: 666,
+      gold: 777
+    }
+  },
+
+
+  // 签到 tapSign 
+  tapSign: function(){
+    if(!this.data.signCheck){
+      this.setData({
+        signIn: true,
+        signCheck: true
+      })
+    }
+  },
+
+  // 关闭签到成功弹框 (朕知道了)
+  signed: function(){
+    this.setData({
+      signIn: false
+    })
+  },
+  
+  
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+   
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
